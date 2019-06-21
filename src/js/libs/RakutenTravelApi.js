0a6a44fbcda99e8cb0cb0667fed146cee51be633
Tomoya Kobayashi <to_kobayashi@gmail.com>
2019-06-21 17:59:57 +0900

    13 - Rakuten Travel API用のクラス作成

--- src/js/libs/RakutenTravelApi.js
+++ src/js/libs/RakutenTravelApi.js
@@ -4,34 +4,14 @@ import ReactDOM from 'react-dom';
 class RakutenTravelApi extends React.Component{
   sendRequest(e){
     e.preventDefault();
-    const url = "https://app.rakuten.co.jp/services/api/Travel/GetHotelChainList/20131024?format=json&applicationId=1045719661893592374";
-
-    // var xhr = new XMLHttpRequest();
-    // xhr.addEventListener("load", ()=>console.log(this.responseText));
-    // xhr.open("GET", url);
-    // xhr.withCredentials = true;
-    // xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
-    // xhr.send();
-
-  //   fetch(url,{
-  //     method: "GET", // *GET, POST, PUT, DELETE, etc.
-  //     mode: "cors", // no-cors, cors, *same-origin
-  //     cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-  //     credentials: "omit", // include, same-origin, *omit
-  //     headers: {
-  //       "Content-Type": "application/json; charset=utf-8",
-  //       "Access-Control-Allow-Origin": "*",
-  //     },
-  //     redirect: "follow", // manual, *follow, error
-  //     referrer: "no-referrer", // no-referrer, *client
-  //   })
-  //   .then(function(response) {
-  //   return response.json();
-  // })
-  // .then(function(myJson) {
-  //   console.log(JSON.stringify(myJson));
-  // });
-
+    // const url = "https://app.rakuten.co.jp/services/api/Travel/VacantHotelSearch/20170426?format=json&checkinDate=2019-06-25&checkoutDate=2019-06-26&latitude=128440.51&longitude=503172.21&searchRadius=3&applicationId=1045719661893592374";
+    let url = "https://app.rakuten.co.jp/services/api/Travel/VacantHotelSearch/20170426?format=json&";
+    let checkinDate = '2019-06-25';
+    let checkoutDate = '2019-06-26';
+    let latitude = '128440.51';
+    let longitude = '503172.21';
+    url += `checkinDate=${checkinDate}&checkoutDate=${checkoutDate}&latitude=${latitude}&longitude=${longitude}`;
+    url += "&searchRadius=3&applicationId=1045719661893592374";
 
       fetch(url)
       .then(function(response) {
@@ -45,7 +25,6 @@ class RakutenTravelApi extends React.Component{
   render(){
     return(
       <button onClick={(e) => this.sendRequest(e)}>RakutenTravelApiのテスト</button>
-      // <button >RakutenTravelApiのテスト</button>
     );
   }
 }
