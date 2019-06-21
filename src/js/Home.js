843f837ef4adb7310d7560381bd5412283b0e38c
Tomoya Kobayashi <to_kobayashi@gmail.com>
2019-06-21 17:12:59 +0900

    ひとまずレスポンスを取得できるようになった

--- src/js/Home.js
+++ src/js/Home.js
@@ -51,6 +51,7 @@ class Home extends React.Component {
             checkInDay={this.state.checkInDay}
             checkOutDay={this.state.checkOutDay}
             />
+          <RakutenTravelApi />
       </div>
     );
   }
--- src/js/libs/RakutenTravelApi.js
+++ src/js/libs/RakutenTravelApi.js
@@ -2,11 +2,51 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class RakutenTravelApi extends React.Component{
+  sendRequest(e){
+    e.preventDefault();
+    const url = "https://app.rakuten.co.jp/services/api/Travel/GetHotelChainList/20131024?format=json&applicationId=1045719661893592374";
+
+    // var xhr = new XMLHttpRequest();
+    // xhr.addEventListener("load", ()=>console.log(this.responseText));
+    // xhr.open("GET", url);
+    // xhr.withCredentials = true;
+    // xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
+    // xhr.send();
+
+  //   fetch(url,{
+  //     method: "GET", // *GET, POST, PUT, DELETE, etc.
+  //     mode: "cors", // no-cors, cors, *same-origin
+  //     cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+  //     credentials: "omit", // include, same-origin, *omit
+  //     headers: {
+  //       "Content-Type": "application/json; charset=utf-8",
+  //       "Access-Control-Allow-Origin": "*",
+  //     },
+  //     redirect: "follow", // manual, *follow, error
+  //     referrer: "no-referrer", // no-referrer, *client
+  //   })
+  //   .then(function(response) {
+  //   return response.json();
+  // })
+  // .then(function(myJson) {
+  //   console.log(JSON.stringify(myJson));
+  // });
+
+
+      fetch(url)
+      .then(function(response) {
+      return response.json();
+    })
+    .then(function(myJson) {
+      console.log(JSON.stringify(myJson));
+    });
+
+  }
   render(){
     return(
-      <button>RakutenTravelApiのテスト</button>
+      <button onClick={(e) => this.sendRequest(e)}>RakutenTravelApiのテスト</button>
+      // <button >RakutenTravelApiのテスト</button>
     );
   }
-  // const url = "https://app.rakuten.co.jp/services/api/Travel/GetHotelChainList/20131024?format=json&applicationId=1045719661893592374";
 }
 export default RakutenTravelApi;
