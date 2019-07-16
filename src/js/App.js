0a0fb1a5484a943ac9585ea67a5a3ca390139ebc
Tomoya Kobayashi <to_kobayashi@gmail.com>
2019-07-16 09:55:12 +0900

    Ru\outing定義の外部移植

--- src/js/App.js
+++ src/js/App.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import DatePicker from 'react-datepicker';
-import SearchForm from './components/SearchForm.js';
-import HotelDetail from './components/HotelDetail.js';
+import SearchForm from './Components/SearchForm.js';
+import HotelDetail from './Components/HotelDetail.js';
 import Home from './Home.js';
-import { Link, Route } from "react-router-dom"
-import {MainRouter} from './components/MainRouter.js';
+import { BrowserRouter, Link, Route } from "react-router-dom"
+import {MainRouter} from './Components/MainRouter.js';
 
 function App(props){
   return(
     <div>
       <Link to='/'><h2>ホーム</h2></Link>
       <Link to='/hotels/160960?id=2'><h2>ホテル</h2></Link>
-      // <MainRouter/>
-      <Route exact path="/" component={Home}></Route>
-      <Route path="/hotels/:id" component={HotelDetail}></Route>
+      <BrowserRouter>
+      <MainRouter/>
+      </BrowserRouter>
     </div>
   );
 }
--- src/js/Components/MainRouter.js
+++ src/js/Components/MainRouter.js
@@ -4,8 +4,11 @@ import HotelDetail from './HotelDetail.js';
 import Home from '../Home.js';
 import { Link, Route, Switch } from "react-router-dom"
 
-export const MainRouter =
-<Switch>
-<Route exact path="/" component={Home}></Route>
-<Route path="/hotels/:id" component={HotelDetail}></Route>
-</Switch>;
+export const MainRouter = () => {
+  return (
+    <Switch>
+    <Route exact path="/" component={Home}></Route>
+    <Route path="/hotels/:id" component={HotelDetail}></Route>
+    </Switch>
+  );
+}
