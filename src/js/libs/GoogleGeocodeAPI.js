eb9ebc4eac6bf34c87079c6ce026196eb612f939
Tomoya Kobayashi <to_kobayashi@gmail.com>
2019-07-25 17:45:21 +0900

    GoogleGeoCodeAPIのリファクタリング

--- src/js/libs/GoogleGeocodeAPI.js
+++ src/js/libs/GoogleGeocodeAPI.js
@@ -1,23 +1,27 @@
+import fetch from 'node-fetch';
+
 export default async function fetchGeoCode(location){
-  let url = "https://map.yahooapis.jp/geocode/V1/geoCoder";
-  url += "?appid=";
-  url += "dj00aiZpPUFjQzFjbjNvemRPayZzPWNvbnN1bWVyc2VjcmV0Jng9MzQ-";
-  url += "&query=";
-  url += location;
-  // url += "東京都千代田区丸の内１丁目";
-  url += "&output=";
-  url += "json";
-  url += "&datum=";
-  url += "wgs";
+  let url = `https://map.yahooapis.jp/geocode/V1/geoCoder`
+  + `?appid=dj00aiZpPUFjQzFjbjNvemRPayZzPWNvbnN1bWVyc2VjcmV0Jng9MzQ-`
+  + `&query=${location}&output=json&datum=wgs`;
 
+  const res = await fetch(encodeURI(url))
+  .catch(() => console.log("fetchに失敗しました"));
 
-  const fetch = require('node-fetch');
-  let res = await fetch(encodeURI(url));
-  await console.log(res);
-  let json = await res.json();
-  let geometry = json.Feature[0].Geometry.Coordinates;
-  geometry = geometry.split(",");
-  let latitude = geometry[0];
-  let longitude = geometry[1];
-  return {"latitude":latitude, "longitude":longitude};
+  if (await res.ok) {
+    const json = await res.json();
+
+    if(json.ResultInfo.Count != 0){
+      let geometry = json.Feature[0].Geometry.Coordinates;
+      geometry = geometry.split(",");
+
+      return {"latitude":geometry[0], "longitude":geometry[1]};
+
+    }else{
+        throw Error("件数が0です");
+    }
+
+  }else{
+    throw Error("fetchGeoCodeのエラー");
+  }
 }
--- test/google.geocode.api.test.js
+++ test/google.geocode.api.test.js
@@ -3,10 +3,14 @@ import fetchGeoCode from '../src/js/libs/GoogleGeocodeAPI.js';
 
 test("住所を入れると緯度と経度が返却される", async()=>{
   let json = await fetchGeoCode("東京都千代田区丸の内１丁目");
-  let latitude = await json.latitude;
-  let longitude = await json.longitude;
-  // expect(latitude).toBe(0);
-  // expect(latitude).not.toBe(0);
-  expect(longitude).toBe(0);
-  // expect(longitude).not.toBe(0);
+  await expect(json).toHaveProperty('latitude');
+  await expect(json).toHaveProperty('longitude');
+});
+
+test("ありえない住所は件数が0エラー", async()=>{
+  try {
+    let json = await fetchGeoCode("東京都千代田区西荻北");
+  } catch (e) {
+    expect(e).toEqual(new Error("fetchGeoCodeのエラー"));
+  }
 });
