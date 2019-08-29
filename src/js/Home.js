027124d85eb0f8777d1e9027a212a0069133127c
Tomoya Kobayashi <to_kobayashi@gmail.com>
2019-08-29 16:20:37 +0900

    機能が減ってきたのでhomeをclassからfunctionに変更

--- src/js/Home.js
+++ src/js/Home.js
@@ -6,38 +6,32 @@ import { Container, Row, Col } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import { setHome, setCheckIn, setCheckOut } from './redux/actions';
 
-class Home extends React.Component {
-  constructor(props){
-    super(props);
-  }
-
-  render(){
-    return (
-      <div className="home pagecontainer">
-        <Row>
-          <Col>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <SearchForm
-              checkInDay={this.props.home.checkInDay}
-              checkOutDay={this.props.home.checkOutDay}
-              handleCheckInChange={this.props.setCheckIn}
-              handleCheckOutChange={this.props.setCheckOut}
-              />
-          </Col>
-          <Col md={8}>
-            <HotelList
-              hotels={this.props.home.hotels}
-              checkInDay={this.props.home.checkInDay}
-              checkOutDay={this.props.home.checkOutDay}
-              />
-          </Col>
-        </Row>
-      </div>
-    );
-  }
+function Home(props){
+  return (
+    <div className="home pagecontainer">
+      <Row>
+        <Col>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <SearchForm
+            checkInDay={props.home.checkInDay}
+            checkOutDay={props.home.checkOutDay}
+            handleCheckInChange={props.setCheckIn}
+            handleCheckOutChange={props.setCheckOut}
+            />
+        </Col>
+        <Col md={8}>
+          <HotelList
+            hotels={props.home.hotels}
+            checkInDay={props.home.checkInDay}
+            checkOutDay={props.home.checkOutDay}
+            />
+        </Col>
+      </Row>
+    </div>
+  );
 }
 
 function mapStateToProps(state){
